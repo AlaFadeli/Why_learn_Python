8714d7f36f3e9303129bda026b2953085d4a38d5
AlaFadeli <alaeddinfadeli@gmail.com>
2024-08-14 13:41:06 +0100

    Numbers count slide

---
+++ scene.py
@@ -0,0 +1,32 @@
+
+from manim import * 
+
+class Count(Animation):
+    def __init__(self, number: DecimalNumber, start : float, end : float, **kwargs) -> None:
+        super().__init__(number, **kwargs)
+
+        self.start = start
+        self.end = end
+
+    def interpolate_mobject(self, alpha: float)-> None:
+        value = self.start + (alpha * (self.end - self.start))
+        self.mobject.set_value(value)
+
+        
+class CountingScene(Scene):
+    def construct(self):
+        number = DecimalNumber().set_color(WHITE).scale(6)
+        number.add_updater(lambda number: number.move_to(ORIGIN))
+
+        self.add(Text("Loading numbers...").move_to(UP * 4))
+        self.add(number)
+
+
+        self.wait()
+
+        self.play(Count(number,0,100), run_time=15, rate_func=linear)
+
+        self.wait()
+
+        
+    
